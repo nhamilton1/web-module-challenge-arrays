789ffd09f03b93ca824b2f3c865a4a8d228b11e6
nhamilton1 <nickh3592@gmail.com>
2021-07-13 20:43:56 -0400

    Still need to do task 5 and 6

--- index.js
+++ index.js
@@ -86,10 +86,11 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(array){
-  
+function addFlavor(array, string){
+  array.unshift(string)
+  return array;
 }
-
+console.log(addFlavor(originalFlavors, "Rainbow Sherbert"));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Houston, we have a problem! There are now 32 flavors in the originalFlavors array! Your task is to remove an item from the end of the array. 
@@ -102,10 +103,11 @@ Use the removeLastFlavor function below to do the following:
   For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
- /*your code here*/
+function removeLastFlavor(array){
+  array.pop();
+  return array;
 }
-
+console.log(removeLastFlavor(originalFlavors))
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -119,10 +121,11 @@ Use the getFlavorByIndex function below to do the following:
   For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-  /*your code here*/
+function getFlavorByIndex(array, ){
+  array.indexOf();
+  return array;
 }
-
+console.log(getFlavorByIndex(originalFlavors))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, 
@@ -139,10 +142,15 @@ Use the removeFlavorByName function below to do the following:
   HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-  /*your code here*/
+function removeFlavorByName(array, string){
+  for (let i = 0; i < array.length; i++){
+    if(array[i].includes(string)){
+      array.splice(1, 1, string)
+      return array;
+    }
+  }
 }
-
+console.log(removeFlavorByName(originalFlavors, "Rocky Road"))
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -165,11 +173,11 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(arr, string){
+function filterByWord(array, string){
   const newArray = [];
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i].includes(string)){
-      newArray.push(arr[i]);
+  for(let i = 0; i < array.length; i++){
+    if(array[i].includes(string)){
+      newArray.push(array[i]);
     }
   }
   return newArray;
